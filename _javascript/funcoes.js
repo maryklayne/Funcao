23caa4be993091dfb4c80afb0c77fad6252de2d2
maryklayne <maryklayne@hotmail.com>
2015-05-27 20:48:54 -0300

    terceiro commit add botoes de ajuda e esconder botoes de envio

--- _javascript/funcoes.js
+++ _javascript/funcoes.js
@@ -4,65 +4,69 @@ function enviarFuncao(valor){
 	if (valor=="botaoDominio"){
 		$("#itr_X").prop('readOnly',false);
 		$("#botaoItrX").prop('disabled',false);
+		$("#ajuda3").show();
+
+
+		
 	}else if(valor=="botaoItrX"){
 		$("#itr_Y").prop('readOnly',false);
 		$("#botaoItrY").prop('disabled',false);
+		$("#ajuda3").toggle();
+		$("#ajuda4").show();
 	}else if(valor=="botaoItrY"){
 		$("#pontos_criticos").prop('readOnly',false);
 		$("#botaoPontoCritico").prop('disabled',false);
+		$("#ajuda4").toggle();
+		$("#ajuda5").show();
 	}else if(valor=="botaoPontoCritico"){
 		$("#maximo").prop('readOnly',false);
 		$("#botaoMaximo").prop('disabled',false);
+		$("#ajuda5").toggle();
+		$("#ajuda6").show();
 	}else if(valor=="botaoMaximo"){
 		$("#minimo").prop('readOnly',false);
 		$("#botaoMinimo").prop('disabled',false);
+		$("#ajuda6").toggle();
+		$("#ajuda7").show();
 	}else if(valor=="botaoMinimo"){
 		$("#pontos_de_inflexao").prop('readOnly',false);
 		$("#botaoPtInflexao").prop('disabled',false);
-	}else{
+		$("#ajuda7").toggle();
+		$("#ajuda8").show();
+	}else if(valor=="botaoPtInflexao"){
 		$("#concavidade").prop('readOnly',false);
 		$("#botaoConcavidade").prop('disabled',false);
+		$("#ajuda8").toggle();
+		$("#ajuda9").show();
 	}
 }
 
 
 $(document).ready(function(){
+	//esconder botões de ajuda
+	for (var i = 3; i <= 9; i++) {
+	    var variavel = '#ajuda'+ i;
+	    $(variavel).toggle();
+	}
+
+	$('.campoTxt').keypress(function(event) {
+
+		if (event.keyCode == '13') {
+			event.preventDefault();  
+			alert("eventoEnter");
+		}
+	});
+	
+	
+	
     $('.btn-func').click(function(){
         enviarFuncao($(this).attr('id'));
-        if (($(this).attr('id')!="botaoDominio")
-        ){
+        if ($(this).attr('id')!="botaoDominio"){
         	$(this).hide(1000);
     	}
     });
+
 });
 
-//function enviarFuncao(valor){
-//	var campo = document.getElementById(valor.id).value;
-//	if (valor.id=="funcao"){
-//		ativar("dominio_funcao");
-//	}else if(valor.id=="dominio_funcao"){			
-//		alert("entrou no 2");
-//		ativar("itr_X");
-//	}else if(valor.id=="itr_X"){		
-//		//alert("entrou no 3");
-//		ativar("itr_Y");
-//	}else if(valor.id=="itr_Y"){		
-//		alert("entrou no 4");
-//		ativar("pontos_criticos");
-//	}else if(valor.id=="pontos_criticos"){		
-//	//	alert("entrou no 4");
-//	//	ativar("pontos_criticos");
-//	}
-//}
-//
-//
-//function ativar(valor){
-//	document.getElementById(valor).readOnly=false;
-//	if(valor=="dominio_funcao"){		
-//		document.getElementById("botaoDominio").disabled=false;
-//	}else if(valor=="itr_X"){
-//		document.getElementById("botaoItrX").disabled=false;
-//	}else if(valor=="itr_Y"){
-//		document.getElementById("botaoItrY").disabled=false;
-//	}
-//}
+
+
