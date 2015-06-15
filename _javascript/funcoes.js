3c3982c78f71b30ddb8e3307b6e6193bb2b0ff22
maryklayne <maryklayne@hotmail.com>
2015-06-14 23:10:06 -0300

    codificação da parte de domínio da função

--- _javascript/funcoes.js
+++ _javascript/funcoes.js
@@ -2,12 +2,11 @@ vari="";
 
 function enviarFuncao(valor){
 	if (valor=="botaoDominio"){
-		
 		$("#itr_X").prop('readOnly',false).focus();
 		$("#botao1").show();
 		$("#ajuda3").show();
 		$("#resp1").show();
-
+		verificaDominio(document.getElementById("funcao").value);
 	}else if(valor=="botao1"){
 		$("#itr_Y").prop('readOnly',false).focus();
 		$("#botao2").show();
@@ -89,5 +88,92 @@ $(document).ready(function(){
     });
 });
 
+
+function verificaDominio(funcao){
+
+	if (funcao.includes('/')){
+		var inicio = funcao.indexOf('/')+1;
+		var fim = funcao.length;
+		var denominador = funcao.substring(inicio,fim);
+		grau(denominador);
+	}else{
+		alert("não tem /");
+	}
+}
+
+function grau(denominador){
+	if(denominador.includes("x^3") || denominador.includes("x³") || denominador.includes("3*x") ){
+		alert("terceiro grau")
+	}else if(denominador.includes("x^2") || denominador.includes("x²") || denominador.includes("2*x") ){
+			alert("segundo grau");
+	}else if(denominador.match(/^[0-9]x/)||denominador.match(/^x[0-9]*/)){
+		alert("primeiro grau");
+		
+		var reg= new RegExp(/(\+|-|\/)/);
+        var entradas = denominador.split(reg);
+		var totalA=0;
+//		alert(entradas);
+		
+		
+		//Calcular totalA
+		for(var i = 0 ; i < entradas.length ; i++){
+//			alert(typeof entradas[i] + entradas[i]);
+			
+			if(entradas[i].match(/^[0-9]+(\.[0-9]+)?$/)){				
+				var valor = parseFloat(entradas[i]);
+				var operador='';
+				if(i==0){
+					operador="+";
+				}else{
+	                operador=entradas[i-1];
+				}
+				if(operador=="+"){
+	            	totalA+=valor;
+	            }else{
+	            	totalA-=valor;
+	            }
+			}
+		}
+//		alert('total de a '+totalA);
+
+		
+		
+		
+//		Calcular totalB
+		var totalB=0;
+		for(var i = 0 ; i < entradas.length ; i++){
+			if(entradas[i].match(/^x[0-9]*|[0-9]+x$/)){
+				var valor = 0;
+				var operador='';
+				if(entradas[i].match(/^x$/)){
+					valor=1;	
+				}else{
+					if(entradas[i].match(/^x/)){
+						valor = parseFloat(entradas[i].split('x')[1]);
+					}else{
+						valor = parseFloat(entradas[i].split('x')[0]);
+					}
+				}
+				if(i==0){
+					operador="+";
+				}else{
+	                operador=entradas[i-1];
+				}
+				if(operador=="+"){
+	            	totalB+=valor;
+	            }else{
+	            	totalB-=valor;
+	            }
+			}
+		}
+		
+		//Calcular domínio
+		alert(totalA);
+		alert(totalB);
+		var num = (totalA/totalB)*-1;
+		alert(num);
+		
+	}	
+}
 
 
